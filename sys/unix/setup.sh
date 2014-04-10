40c0b912a5aa453c083801f4620697ac5fa981f8
Brian Chin <naerbnic@ucla.edu>
2014-04-09 21:15:30 -0700

    Add all unix makefiles for simplicity.

--- sys/unix/setup.sh
+++ sys/unix/setup.sh
@@ -10,25 +10,6 @@
 # Were we started from the top level?  Cope.
 if [ -f sys/unix/Makefile.top ]; then cd sys/unix; fi
 
-if [ $# -gt 0 ] ; then
-#	First, try to make a symbolic link.
-#
-	ln -s Makefile.top Makefile >/dev/null 2>&1
-	if [ $? -eq 0 ] ; then
-
-		echo "Lucky you!  Symbolic links."
-		rm -f Makefile
-
-		umask 0
-		ln -s sys/unix/Makefile.top ../../Makefile
-		ln -s ../sys/unix/Makefile.dat ../../dat/Makefile
-		ln -s ../sys/unix/Makefile.doc ../../doc/Makefile
-		ln -s ../sys/unix/Makefile.src ../../src/Makefile
-		ln -s ../sys/unix/Makefile.utl ../../util/Makefile
-		exit 0
-	fi
-fi
-
 #
 #	Otherwise...
 
