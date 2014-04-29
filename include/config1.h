88bb4b3fdab75d013b5c6f598ea30d7fb7a54106
Brian Chin <naerbnic@ucla.edu>
2014-04-29 10:01:57 -0700

    Removed unnecessary conf files from includes.

--- include/config1.h
+++ include/config1.h
@@ -5,39 +5,4 @@
 #ifndef CONFIG1_H
 #define CONFIG1_H
 
-/*
- * MS DOS - compilers
- *
- * Microsoft C auto-defines MSDOS,
- * Borland C   auto-defines __MSDOS__,
- * DJGPP       auto-defines MSDOS.
- */
-
-
-#ifdef vax
-/* just in case someone thinks a DECstation is a vax. It's not, it's a mips */
-# ifdef ULTRIX_PROTO
-#  undef ULTRIX_PROTO
-# endif
-# ifdef ULTRIX_CC20
-#  undef ULTRIX_CC20
-# endif
-#endif
-
-#ifdef KR1ED		/* For compilers which cannot handle defined() */
-#define defined(x) (-x-1 != -1)
-/* Because:
- * #define FOO => FOO={} => defined( ) => (-1 != - - 1) => 1
- * #define FOO 1 or on command-line -DFOO
- *	=> defined(1) => (-1 != - 1 - 1) => 1
- * if FOO isn't defined, FOO=0. But some compilers default to 0 instead of 1
- * for -DFOO, oh well.
- *	=> defined(0) => (-1 != - 0 - 1) => 0
- *
- * But:
- * defined("") => (-1 != - "" - 1)
- *   [which is an unavoidable catastrophe.]
- */
-#endif
-
 #endif	/* CONFIG1_H */
