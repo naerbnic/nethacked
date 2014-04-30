75d3a1a9596c424f5dd5391fd5c875a646f89e22
Brian Chin <naerbnic@ucla.edu>
2014-04-30 10:34:49 -0700

    More changes.

--- scripts/rem_decl.py
+++ scripts/rem_decl.py
@@ -6,7 +6,6 @@ def main(args):
         process_file(file_name)
 
 def find_matching_close(s, index):
-    assert s[index] == '('
     depth = 1
     curr_index = index + 1
     while curr_index < len(s) and depth > 0:
@@ -21,13 +20,17 @@ def find_matching_close(s, index):
         return -1
 
 argdecl_re = re.compile(r'\b(F|V)DECL\(')
-ndecl_re = re.compile(r'\bNDECL\((?P<name>[^\)]+)\)')
+ndecl_re = re.compile(r'\bNDECL\(')
 
 def process_lines(lines_arg):
     lines = lines_arg[:]
     out_lines = []
     for line in lines:
-        out_lines.append(ndecl_re.sub(r'\g<name>()', line))
+        m = ndecl_re.match(line);
+        if m:
+            actual_end = find_matching_close(line, m.end(0))
+        out_lines.append(line);
+        
     return out_lines
 
 def process_file(file_name):
