890e3d9b58c6af8dd9e72e329ad7ea9b3cf953d3
Brian Chin <naerbnic@ucla.edu>
2014-05-02 23:24:30 -0700

    Added LinkedList class.

---
+++ cxx/listiter.h
@@ -0,0 +1,39 @@
+#ifndef LISTITER_H_
+#define LISTITER_H_
+
+template <typename T, typename NextFunc>
+class LinkedList {
+  LinkedList(T** head, NextFunc next_func = NextFunc())
+      : head_(head), next_func_(next_func) {}
+
+  class iterator {
+  public:
+    iterator(T** curr) : curr_(curr) {}
+
+    T* operator*() {
+      return *curr_;
+    }
+
+    bool operator!=(iterator const& other) {
+      if (curr_ == other.curr_) {
+        return false;
+      }
+
+      return *curr_ != *other.curr_;
+    }
+
+    iterator& operator++() {
+      curr_ = next_func_(*curr_);
+      return *this;
+    }
+
+  private:
+    T** curr_;
+  };
+
+private:
+  T** head_;
+  NextFunc next_func_;
+};
+
+#endif  // LISTITER_H_
