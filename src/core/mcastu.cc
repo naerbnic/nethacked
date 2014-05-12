52e61ba0852591c2e1af4fad863687b4a9e7e0d9
Brian Chin <naerbnic@ucla.edu>
2014-05-12 10:36:17 -0700

    Fixed mcastu

--- src/core/mcastu.cc
+++ src/core/mcastu.cc
@@ -2,6 +2,8 @@
 /* Copyright (c) Stichting Mathematisch Centrum, Amsterdam, 1985. */
 /* NetHack may be freely redistributed.  See license for details. */
 
+#include "core/mcastu.h"
+
 #include "core/hack.h"
 #include "core/zap.h"
 #include "core/worn.h"
