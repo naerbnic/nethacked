769675742b60fec727fb5d9870844b0e6689296c
Brian Chin <naerbnic@ucla.edu>
2014-05-12 15:34:35 -0700

    Comment out some code that's necessary.

--- src/cxx/test_main.cc
+++ src/cxx/test_main.cc
@@ -18,23 +18,23 @@ int main(int argc, char const* argv[]) {
   cout << StrFormat("%s, %s!\n", "Hello", "World");
   //cout << StrFormat("%s, %s!\n %s\n", "Hello", "World");
 
-  std::string json_with_comments = R"START(
-[
-  "foo//", // This
-  "bar/*", // Is
-  "baz", /*/ A */ "quux" // Test
-]
-)START";
-
-  std::string result;
-  if (!JSONStripComments(json_with_comments, &result)) {
-    cout << "ERROR: Cannot parse JSON file!";
-  }
-
-  cout << result << endl;
-
-  json_spirit::Value parse_result;
-  json_spirit::read(result, parse_result);
-
-  cout << parse_result.get_array()[0].get_str() << std::endl;
+//  std::string json_with_comments = R"START(
+//[
+//  "foo//", // This
+//  "bar/*", // Is
+//  "baz", /*/ A */ "quux" // Test
+//]
+//)START";
+//
+//  std::string result;
+//  if (!JSONStripComments(json_with_comments, &result)) {
+//    cout << "ERROR: Cannot parse JSON file!";
+//  }
+//
+//  cout << result << endl;
+//
+//  json_spirit::Value parse_result;
+//  json_spirit::read(result, parse_result);
+//
+//  cout << parse_result.get_array()[0].get_str() << std::endl;
 }
