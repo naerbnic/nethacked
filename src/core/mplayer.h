f7f7485022ce6c07d45008fd319b6015807418e8
Brian Chin <naerbnic@ucla.edu>
2014-05-12 14:25:24 -0700

    Fixed mplayer

--- src/core/mplayer.h
+++ src/core/mplayer.h
@@ -3,6 +3,10 @@
 #ifndef MPLAYER_H_
 #define MPLAYER_H_
 
+#include "core/global.h"
+#include "core/monst.h"
+#include "core/permonst.h"
+
 Monster *mk_mplayer(MonsterType *, xchar, xchar, bool);
 void create_mplayers(int, bool);
 void mplayer_talk(Monster *);
